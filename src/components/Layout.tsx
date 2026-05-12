79c6d10b5d61eed76e80f8dd0bea23cabeb719db
Priyanshu Thakare <anshulthakare83@gmail.com>
2026-05-12 20:39:39 +0530

    ingret

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router';
 import Navigation from './Navigation';
 import Footer from './Footer';
+import { Databuddy } from '@databuddy/sdk/react';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const location = useLocation();
@@ -15,6 +16,12 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <Navigation />
       {children}
       <Footer />
+      <Databuddy
+        clientId={import.meta.env.VITE_DATABUDDY_CLIENT_ID || "a685c18e-c702-4839-b963-ccec1436c7f6"}
+        trackInteractions={true}
+        trackWebVitals
+        trackErrors
+      />
     </>
   );
 }
